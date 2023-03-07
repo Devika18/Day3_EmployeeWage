7f557cf40235755512fab0e17e754185247e9f78
devikakarpe <devikankarpe@gmail.com>
2023-03-06 20:04:32 -0800

    added logic for calculating month wage

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,25 @@ import java.util.Random;
 
 public class Main {
 
-    public static final int partTime = 2;
-    public static final int fullTime = 1;
-    public static final int h = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
 
         Random random = new Random();
-        int empHrs, empWage;
-        int randomCheck = random.nextInt(4);
-        System.out.println("Random value is: "+ randomCheck);
-        switch (randomCheck) {
-            case fullTime:
-                empHrs = 8;
-                break;
-            case partTime:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        System.out.println("Random value is: ");
+        int day = 20;
+        int w = 20;
+        int fullTime = 10;
+        int salary, totalSalary = 0;
+        int isPresent = random.nextInt(5);
+        System.out.println(isPresent);
+
+        if (isPresent>2) {
+            salary = w * fullTime * day;
+            totalSalary = totalSalary + salary;
+            System.out.println("Monthly salary of the employee is: " + totalSalary);
+        } else {
+            salary = 0;
+            System.out.println("Employee is absent and the salary is: " + salary);
         }
-        empWage = (empHrs * h);
-        System.out.println("Employee salary is:"+empWage);
     }
 }
