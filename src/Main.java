8d0869a01083d3c7ff1730fb11428bc4ca3c8beb
devikakarpe <devikankarpe@gmail.com>
2023-03-06 19:54:28 -0800

    added logic for employee attendance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
+
+        System.out.println("Random value is: ");
+        Random random = new Random();
+        int r = random.nextInt(5);
+        System.out.println(r);
+        if(r>3) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
